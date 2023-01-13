2e61531dcf97bb9cbffc24c4c99cc3a39d41220b
Abilash S L <abilash0045@gmail.com>
2023-01-13 13:38:04 +0530

    soln2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B b = new B();
-        b.meth();
+        B obj = new B();
+        obj.meth();
     }
 }
